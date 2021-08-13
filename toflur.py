3b8880fc32cd20bd9196a8249a224ec28bb950ac
Daniyal Arteshdar <82860136+DaniyalArteshdar@users.noreply.github.com>
2021-08-13 12:25:17 -0700

    Create toflur.py

---
+++ toflur.py
@@ -0,0 +1,10 @@
+# Kattis-Python-Solutions
+# Coded by Daniyal Arteshdar
+numberOfTiles = int(input())
+tiles = list(map(int, input().split()))
+tiles.sort()
+score = 0
+for tile1, tile2 in zip(tiles[:-1], tiles[1:]):
+    score += (tile1 - tile2) ** 2
+
+print(score)
