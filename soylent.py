67de85cf6b2d06cbd51f6c23d13d88a3bc479eee
Daniyal Arteshdar <82860136+DaniyalArteshdar@users.noreply.github.com>
2021-08-13 12:42:29 -0700

    Create soylent.py

---
+++ soylent.py
@@ -0,0 +1,10 @@
+# Kattis-Python-Solutions
+# Coded by Daniyal Arteshdar
+import math
+
+caloriesPerBottle = 400
+numberOfTests = int(input())
+for _ in range(numberOfTests):
+    numberOfCaloriesPerDay = int(input())
+    numberOfBottles = int(math.ceil(numberOfCaloriesPerDay / caloriesPerBottle))
+    print(numberOfBottles)
