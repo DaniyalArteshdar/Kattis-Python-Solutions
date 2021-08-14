69389080bd3e6f0f66dfaf5a535e1b0ad641fd70
Daniyal Arteshdar <82860136+DaniyalArteshdar@users.noreply.github.com>
2021-08-14 10:15:52 -0700

    Create stopwatch.py

---
+++ stopwatch.py
@@ -0,0 +1,18 @@
+# Kattis-Python-Solutions
+# Coded Daniyal Arteshdar 
+numberOfPresses = int(input())
+isRunning = False
+accumulatedTime = 0
+previousTime = 0
+for _ in range(numberOfPresses):
+    currentTime = int(input())
+    if isRunning:
+        accumulatedTime += currentTime - previousTime
+
+    previousTime = currentTime
+    isRunning = not isRunning
+
+if isRunning:
+    print("still running")
+else:
+print(accumulatedTime)
