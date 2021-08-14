23b9abdc126aa23200c96cfc000b55fcc7dfae82
Daniyal Arteshdar <82860136+DaniyalArteshdar@users.noreply.github.com>
2021-08-14 10:14:29 -0700

    Create jumbojavelin.py

---
+++ jumbojavelin.py
@@ -0,0 +1,12 @@
+# Kattis-Python-Solutions
+# Coded Daniyal Arteshdar 
+numberOfRods = int(input())
+totalLength = 0
+for _ in range(numberOfRods):
+    rodLength = int(input())
+    if totalLength == 0:
+        totalLength += 1
+
+    totalLength += rodLength - 1
+
+print(totalLength)
