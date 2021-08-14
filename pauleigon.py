383f2e696985fdc428c255d78ce403f730041ac8
Daniyal Arteshdar <82860136+DaniyalArteshdar@users.noreply.github.com>
2021-08-14 10:17:23 -0700

    Create pauleigon.py

---
+++ pauleigon.py
@@ -0,0 +1,17 @@
+# Kattis-Python-Solutions
+# Coded Daniyal Arteshdar 
+import sys
+
+inputstd = sys.stdin.read()
+values = inputstd.strip("\n")
+values = values.split()
+
+servers = ["paul", "opponent"]
+result = 0
+n = int(values[1])
+m = int(values[2])
+p = int(values[0])
+
+nm = n + m
+result  = int((float(nm)/p) % 2)
+print (servers[result])
