5046f4b2ae290bcf795644d96995e1fa3ad5af2e
Daniyal Arteshdar <82860136+DaniyalArteshdar@users.noreply.github.com>
2021-08-26 12:55:54 -0700

    Create quickestimate.py

---
+++ quickestimate.py
@@ -0,0 +1,13 @@
+# Kattis-Python-Solutions
+# Coded by Daniyal Arteshdar
+
+import sys
+
+inputstd = sys.stdin.read()
+
+line = inputstd.replace("\n", " ")
+values = line.split(" ")
+values = values[1:-1]
+
+for item in values:
+    print (len(item))
