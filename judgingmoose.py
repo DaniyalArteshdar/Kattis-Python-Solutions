21926223f93d1ecfb37bee2943f3a777870777c6
Daniyal Arteshdar <82860136+DaniyalArteshdar@users.noreply.github.com>
2021-12-07 05:09:41 -0800

    Create judgingmoose.py

---
+++ judgingmoose.py
@@ -0,0 +1,11 @@
+# Kattis-Python-Solutions
+# Coded by Daniyal Arteshdar (Py.Dan)
+
+l,r = map(int,input().split())
+
+if l==r and l!=0 and r!=0:
+    print('Even {}'.format(2*l))
+elif l == 0 == r:
+    print('Not a moose')
+else:
+    print('Odd {}'.format(2*max(l,r)))
